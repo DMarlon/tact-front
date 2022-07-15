2cff339ac1a22be2f27c179b3d9b3fd88154f3b0
Marlon Dauernheimer <marlon.dauernheimer@gmail.com>
2022-07-15 18:57:07 -0300

    feat: added custom project name in navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="navbar-main w-100">
        <b-navbar type="dark" variant="info">
-            <b-navbar-brand><h2><b>Tact.com</b></h2></b-navbar-brand>
+            <b-navbar-brand><h2><b>{{ projectName }}</b></h2></b-navbar-brand>
         </b-navbar>
     </div>
 </template>
 
 <script>
 export default {
-    name: "NavBar"
+    name: "NavBar",
+    computed: {
+        projectName() {
+            return process.env.VUE_APP_FRONT_NAME
+        }
+    }
+
 }
 </script>
